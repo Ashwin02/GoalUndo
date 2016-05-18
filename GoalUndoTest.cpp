8c57c6c9329c1fb2acfd60e16a2dfa4b6475f617
Ashwin02 <ashwin.s.patil10@gmail.com>
2016-05-17 20:39:03 -0700

    Test cases for rest of the functions

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -92,3 +92,123 @@ TEST(GoalUndoTest, addOperation2BlankGoalNameCheck)
   obj.addOperation("op1");
 	ASSERT_EQ("op1",obj.getGoal());
 }
+
+
+TEST(GoalUndoTest, undoOpertaionCheck)
+{
+  GoalUndo obj;
+  obj.addOperation("goal1","op1");
+  obj.undoOperation();
+	ASSERT_EQ("",obj.getOperations());
+}
+
+TEST(GoalUndoTest, undoOpertaionNoGoalCheck)
+{
+  GoalUndo obj;
+  //obj.addOperation("","");
+  obj.undoOperation();
+	ASSERT_EQ("",obj.getOperations());
+}
+
+TEST(GoalUndoTest, undoOpertaionNoOpCheck)
+{
+  GoalUndo obj;
+  obj.addOperation("op1");
+  obj.undoOperation();
+	ASSERT_EQ("",obj.getOperations());
+	ASSERT_EQ("",obj.getGoal());
+}
+
+
+
+TEST(GoalUndoTest, getGoalCheck)
+{
+  GoalUndo obj;
+  obj.addOperation("goal1","op1");
+	obj.addOperation("goal2","op2");
+	ASSERT_EQ("goal2",obj.getGoal());
+}
+
+TEST(GoalUndoTest, getGoalNoGoalCheck)
+{
+  GoalUndo obj;
+	ASSERT_EQ("",obj.getGoal());
+}
+
+TEST(GoalUndoTest, getOperationsCheck)
+{
+  GoalUndo obj;
+	ASSERT_EQ("",obj.getOperations());
+}
+
+TEST(GoalUndoTest, getOperationsWithOneOpCheck)
+{
+  GoalUndo obj;
+	obj.addOperation("goal1","op1");
+	ASSERT_EQ("op1",obj.getOperations());
+}
+
+TEST(GoalUndoTest, getOperationsWithOpsCheck)
+{
+  GoalUndo obj;
+	obj.addOperation("goal1","op1");
+	obj.addOperation("op2");
+	obj.addOperation("op3");
+	ASSERT_EQ("op1 op2 op3",obj.getOperations());
+}
+
+TEST(GoalUndoTest, normalUndoOpertaion2Check)
+{
+  GoalUndo obj;
+  obj.addOperation("goal1","op1");
+  obj.undoOperation("op1");
+	ASSERT_EQ("",obj.getOperations());
+
+}
+
+TEST(GoalUndoTest, undoOpertaion2OnlyGoalCheck)
+{
+  GoalUndo obj;
+  obj.addOperation("goal1","op1");
+  obj.undoOperation("op1");
+	ASSERT_EQ("",obj.getGoal());
+}
+
+TEST(GoalUndoTest, undoOpertaion2NoGoalCheck)
+{
+  GoalUndo obj;
+//  obj.addOperation("goal1","op1");
+//  obj.undoOperation("op1");
+	ASSERT_EQ("",obj.getGoal());
+}
+
+TEST(GoalUndoTest, undoOpertaion2OpsCheck)
+{
+  GoalUndo obj;
+  obj.addOperation("goal1","op1");
+  obj.addOperation("op2");
+	obj.undoOperation("op1");
+	ASSERT_EQ("op2",obj.getOperations());
+}
+
+TEST(GoalUndoTest, undoGoalCheck)
+{
+  GoalUndo obj;
+	obj.addOperation("goal1","op1");
+	obj.undoGoal();
+	ASSERT_EQ("",obj.getGoal());
+}
+
+TEST(GoalUndoTest, undoGoalOpsCheck)
+{
+  GoalUndo obj;
+	obj.addOperation("goal1","op1");
+	obj.undoGoal();
+	ASSERT_EQ("",obj.getOperations());
+}
+TEST(GoalUndoTest, undoGoalNoGoalCheck)
+{
+  GoalUndo obj;
+	obj.undoGoal();
+	ASSERT_EQ("",obj.getOperations());
+}
